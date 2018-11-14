b42e6e820981bd5975d2f8d073010311eb5d3c43
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-11-13 16:52:20 -0800

    Add the initial commandline tool

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "telemetry-schema-transpiler",
-    version = "0.1",
-    packages = find_packages(),
-    install_requires=[],
-    extras_require={
-        "test": ["pytest"]
-    }
+    name="telemetry-schema-transpiler",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["click"],
+    extras_require={"test": ["pytest"]},
 )
