36438c001eefac97787b037342bcada5a4826d17
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-01-15 16:24:21 -0800

    Remove python code

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="telemetry-schema-transpiler",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=["click"],
-    extras_require={"test": ["pytest"]},
-)
