5c89a1e40ba8e55b8f0ee7d1cba1ecfd0624a09e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-01-17 15:58:07 -0800

    Remove simple test from main.rs

--- src/main.rs
+++ src/main.rs
@@ -25,34 +25,3 @@ use serde_json::Value;
 fn main() {
     println!("hello world!");
 }
-
-#[test]
-fn test_simple() {
-    let data = r#"
-    {
-        "type": "object",
-        "properties": {
-            "field_1": {"type": "integer"},
-            "field_2": {"type": "string"},
-            "field_3": {"type": "boolean"}
-        },
-        "required": ["field_1", "field_3"]
-    }"#;
-
-    let v: Value = serde_json::from_str(data).unwrap();
-
-    let expect = r#"
-    {
-        "type": "record",
-        "name": "root",
-        "fields": [
-            {"name": "field_1", "type": "int"},
-            {"name": "field_2", "type": "string"},
-            {"name": "field_3", "type": "boolean"}
-        ]
-    }
-    "#;
-    let v1: Value = serde_json::from_str(expect).unwrap();
-
-    assert_eq!(v1, convert_avro_direct(&v, "root".to_string()));
-}
