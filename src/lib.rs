a1eb0f7ab1dae804caa658d2b1f4b032aced58dd
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-01-25 16:06:56 -0800

    Refactor individual cases into functions for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub fn convert_avro_direct(input: &Value, name: String) -> Value {
     json!(element)
 }
 
-fn match_simple_bq_types(dtype: &str, ctx: &Value) -> (String, String) {
+fn match_bq_atomic_type(dtype: &str, ctx: &Value) -> (String, String) {
     // arrays are a special-case that should be returned when seen
     if dtype == "array" {
         // TODO: what happens to arrays of optional integers?
@@ -48,75 +48,75 @@ fn match_simple_bq_types(dtype: &str, ctx: &Value) -> (String, String) {
     }
 }
 
+fn match_bq_multi_type(vec: &Vec<Value>, ctx: &Value) -> (String, String) {
+    let mut set: HashSet<&str> = HashSet::from_iter(vec.into_iter().map(|x| x.as_str().unwrap()));
+    let mode = if set.contains("null") {
+        set.remove("null");
+        "NULLABLE"
+    } else {
+        "REQUIRED"
+    };
+    let dtype = if set.len() > 1 {
+        "STRING".into()
+    } else {
+        let rest = set.iter().next().unwrap();
+        let (dtype, _) = match_bq_atomic_type(rest, ctx);
+        dtype
+    };
+    (dtype, mode.into())
+}
+
+fn handle_record(
+    properties: &Map<String, Value>,
+    dtype: String,
+    mode: String,
+    ctx: &Value,
+) -> Value {
+    let required: HashSet<String> = match ctx["required"].as_array() {
+        Some(array) => HashSet::from_iter(
+            array
+                .to_vec()
+                .into_iter()
+                .map(|v| v.as_str().unwrap().to_string()),
+        ),
+        None => HashSet::new(),
+    };
+    let mut fields: Vec<Value> = Vec::from_iter(properties.into_iter().map(|(k, v)| {
+        let mut field: Value = convert_bigquery_direct(v);
+        let mode: String = field["mode"].as_str().unwrap().into();
+
+        // create a mutable reference to the processed value
+        let object = field.as_object_mut().unwrap();
+        object.insert("name".to_string(), json!(k));
+
+        // ignore the mode of the field unless defined in `required` keyword field
+        if mode != "REPEATED" {
+            if required.contains(k) && mode != "NULLABLE" {
+                object.insert("mode".to_string(), json!("REQUIRED"));
+            } else {
+                object.insert("mode".to_string(), json!("NULLABLE"));
+            }
+        }
+        json!(object)
+    }));
+    fields.sort_by_key(|x| x["name"].as_str().unwrap().to_string());
+    json!({
+        "type": dtype,
+        "mode": mode,
+        "fields": fields,
+    })
+}
+
 /// Convert JSONSchema into a BigQuery compatible schema
-///
-/// ## Notes:
-/// It's probably useful to pass the entire subtree to the current node in the
-/// tree in order to make sense of context.
 pub fn convert_bigquery_direct(input: &Value) -> Value {
     let (dtype, mode): (String, String) = match &input["type"] {
-        // if the type is a string, the mapping is straightforward
-        Value::String(dtype) => match_simple_bq_types(dtype.as_str(), input),
-        // handle multi-types
-        Value::Array(vec) => {
-            let mut set: HashSet<&str> =
-                HashSet::from_iter(vec.into_iter().map(|x| x.as_str().unwrap()));
-            let mode = if set.contains("null") {
-                set.remove("null");
-                "NULLABLE"
-            } else {
-                "REQUIRED"
-            };
-            let dtype = if set.len() > 1 {
-                "STRING".into()
-            } else {
-                let rest = set.iter().next().unwrap();
-                let (dtype, _) = match_simple_bq_types(rest, input);
-                dtype
-            };
-            (dtype, mode.into())
-        }
+        Value::String(dtype) => match_bq_atomic_type(dtype.as_str(), input),
+        Value::Array(vec) => match_bq_multi_type(vec, input),
         _ => panic!(),
     };
     if dtype == "RECORD" {
         match &input["properties"].as_object() {
-            Some(properties) => {
-                let required: HashSet<String> = match input["required"].as_array() {
-                    Some(array) => HashSet::from_iter(
-                        array
-                            .to_vec()
-                            .into_iter()
-                            .map(|v| v.as_str().unwrap().to_string()),
-                    ),
-                    None => HashSet::new(),
-                };
-                let mut fields: Vec<Value> =
-                    Vec::from_iter(properties.into_iter().map(|(k, v)| {
-                        let mut field: Value = convert_bigquery_direct(v);
-                        let mode: String = field["mode"].as_str().unwrap().into();
-
-                        // create a mutable reference to the processed value
-                        let object = field.as_object_mut().unwrap();
-                        object.insert("name".to_string(), json!(k));
-
-                        // ignore the mode of the field unless defined in `required` keyword field
-                        if mode != "REPEATED" {
-                            if required.contains(k) && mode != "NULLABLE" {
-                                object.insert("mode".to_string(), json!("REQUIRED"));
-                            } else {
-                                object.insert("mode".to_string(), json!("NULLABLE"));
-                            }
-                        }
-                        // return this record
-                        json!(object)
-                    }));
-                fields.sort_by_key(|x| x["name"].as_str().unwrap().to_string());
-                json!({
-                    "type": dtype,
-                    "mode": mode,
-                    "fields": fields,
-                })
-            }
+            Some(properties) => handle_record(properties, dtype, mode, input),
             None => {
                 unimplemented!();
             }
