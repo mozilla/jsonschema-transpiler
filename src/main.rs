59fb0946374644e82866a08502ed7a3b4be7c394
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-02-04 10:57:54 -0800

    Add clap and initial app to project

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-// Documentation references:
-// https://docs.serde.rs/serde_json/value/index.html
-// https://doc.rust-lang.org/1.30.0/book/second-edition/ch08-03-hash-maps.html
-// http://json-schema.org/learn/miscellaneous-examples.html
-// https://avro.apache.org/docs/1.8.1/spec.html#schema_primitive
-// https://users.rust-lang.org/t/how-to-iterate-over-json-objects-with-hierarchy/19632
-
-// #[derive(Serialize, Deserialize)]
-// struct JsonSchemaField {
-//     type: String,
-//     properties: Option<HashMap<String, Box<JsonSchemaField>>>,
-//     required: Option<Vec<String>>,
-// }
-
-// #[derive(Serialize, Deserialize)]
-// struct AvroField {
-//     type: String,
-//     name: String,
-//     fields: Option<List<Box<AvroField>>>
-// }
+extern crate clap;
 extern crate converter;
 
+use clap::{App, Arg};
+
 fn main() {
-    println!("hello world!");
+    let matches = App::new("jst")
+        .version("0.1")
+        .author("Anthony Miyaguchi <amiyaguchi@mozilla.com>")
+        .arg(
+            Arg::with_name("from_file")
+                .short("f")
+                .long("from-file")
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    println!("Value of input: {}", matches.value_of("from_file").unwrap());
 }
