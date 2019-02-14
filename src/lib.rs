62d13d175cadbf99b42afd1d478e842684596036
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-02-13 17:22:36 -0800

    Make library wasm compatible

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@
 extern crate serde;
 extern crate serde_json;
 
+use wasm_bindgen::prelude::*;
+
 mod ast;
 mod bigquery;
 mod jsonschema;
 
-use serde_json::{json, Value};
-
 /// Convert JSONSchema into a BigQuery compatible schema
-pub fn convert_bigquery(input: &Value) -> Value {
-    let jsonschema: jsonschema::Tag = serde_json::from_value(json!(input)).unwrap();
+#[wasm_bindgen]
+pub fn convert_bigquery(input: &JsValue) -> JsValue {
+    let jsonschema: jsonschema::Tag = input.into_serde().unwrap();
     let ast = ast::Tag::from(jsonschema);
     let bq = bigquery::Tag::from(ast);
-    json!(bq)
+    JsValue::from_serde(&bq).unwrap()
 }
