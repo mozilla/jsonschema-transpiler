d000c6ed8e88fabe7b63c73a51ed05b344612521
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-02-15 14:34:33 -0800

    Add avro conversion to cli

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,19 @@ mod jsonschema;
 
 use serde_json::{json, Value};
 
-/// Convert JSONSchema into a BigQuery compatible schema
-pub fn convert_bigquery(input: &Value) -> Value {
+fn into_ast(input: &Value) -> ast::Tag {
     let jsonschema: jsonschema::Tag = serde_json::from_value(json!(input)).unwrap();
-    let ast = ast::Tag::from(jsonschema);
-    let bq = bigquery::Tag::from(ast);
+    ast::Tag::from(jsonschema)
+}
+
+/// Convert JSON Schema into an Avro compatible schema
+pub fn convert_avro(input: &Value) -> Value {
+    let avro = avro::Type::from(into_ast(input));
+    json!(avro)
+}
+
+/// Convert JSON Schema into a BigQuery compatible schema
+pub fn convert_bigquery(input: &Value) -> Value {
+    let bq = bigquery::Tag::from(into_ast(input));
     json!(bq)
 }
