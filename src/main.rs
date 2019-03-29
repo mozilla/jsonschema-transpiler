04b44d94651850194750c6db3ec1a48ea4a12c0f
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-03-29 11:32:34 -0700

    Update package name and arguments

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let matches = App::new("jst")
-        .version("0.1")
-        .author("Anthony Miyaguchi <amiyaguchi@mozilla.com>")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("from-file")
                 .short("f")
@@ -20,10 +21,11 @@ fn main() {
         )
         .arg(
             Arg::with_name("type")
+                .short("t")
                 .long("type")
                 .takes_value(true)
                 .possible_values(&["avro", "bigquery"])
-                .required(true),
+                .default_value("avro")
         )
         .get_matches();
 
@@ -35,7 +37,7 @@ fn main() {
     let output = match matches.value_of("type").unwrap() {
         "avro" => jst::convert_avro(&data),
         "bigquery" => jst::convert_bigquery(&data),
-        _ => panic!(),
+        _ => panic!("Unknown type!"),
     };
     let pretty = serde_json::to_string_pretty(&output).unwrap();
     println!("{}", pretty);
